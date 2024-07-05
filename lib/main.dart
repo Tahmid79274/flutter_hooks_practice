94a13287de4ac2105165ef6b54a6f5a9c1b3cc85
Tahmid. <tahmidur.rahman.79274@gmail.com>
2024-07-05 23:44:59 +0600

    Application of useMemoized and useListenable

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([
-    E? Function(T?)? transform,
-  ]) =>
-      map(transform ?? (e) => e).where((e) => e != null).cast();
-}
-
-const url = 'https://rb.gy/oro9tn';
 
 void main() {
   runApp(const MyApp());
@@ -31,31 +23,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(
+      const Duration(seconds: 1),
+      (computationCount) => from - computationCount,
+    )
+        .takeWhile(
+      (element) => element >= 0,
+    )
+        .listen(
+      (value) {
+        this.value = value;
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
+
 class MyHomePage extends HookWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // final image = useFuture();
-
-    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
-        .load(url)
-        .then(
-          (value) => value.buffer.asUint8List(),
-        )
-        .then(
-          (value) => Image.memory(value),
-        ));
-
-    final snapshot = useFuture(future);
+    final countDown = useMemoized(
+      () => CountDown(from: 100),
+    );
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Hooks'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [snapshot.data].compactMap().toList(),
+      body: Center(
+        child: Text(
+          notifier.value.toString(),
+          style: TextStyle(fontSize: 100),
+        ),
       ),
     );
   }

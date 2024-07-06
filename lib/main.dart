3f7541bdbe65cd48071218cf0cbf86f1b15023a2
Tahmid. <tahmidur.rahman.79274@gmail.com>
2024-07-06 20:15:09 +0600

    Application of useController

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -23,28 +24,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CountDown extends ValueNotifier<int> {
-  late StreamSubscription sub;
-  CountDown({required int from}) : super(from) {
-    sub = Stream.periodic(
-      const Duration(seconds: 1),
-      (computationCount) => from - computationCount,
-    )
-        .takeWhile(
-      (element) => element >= 0,
-    )
-        .listen(
-      (value) {
-        this.value = value;
-      },
-    );
-  }
+final url = 'https://rb.gy/ggnngr';
+const imgHeight = 300;
 
-  @override
-  void dispose() {
-    sub.cancel();
-    super.dispose();
-  }
+extension Normalize on num {
+  num normalized(num selfRangeMin, num selfRangeMax,
+          [num normalizedRangeMin = 0.0, num normalizedRangeMax = 1.0]) =>
+      (normalizedRangeMax - normalizedRangeMin) *
+          ((this - selfRangeMin) / (selfRangeMax - normalizedRangeMax)) +
+      normalizedRangeMin;
 }
 
 class MyHomePage extends HookWidget {
@@ -52,20 +40,64 @@ class MyHomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final countDown = useMemoized(
-      () => CountDown(from: 100),
-    );
-    final notifier = useListenable(countDown);
+    final opacity = useAnimationController(
+        duration: const Duration(seconds: 1),
+        initialValue: 1.0,
+        upperBound: 1.0,
+        lowerBound: 0.0);
+
+    final size = useAnimationController(
+        duration: const Duration(seconds: 1),
+        initialValue: 1.0,
+        upperBound: 1.0,
+        lowerBound: 0.0);
+
+    final controller = useScrollController();
+
+    useEffect(() {
+      controller.addListener(() {
+        print('Add Listener Called');
+        final newOpacity = max(imgHeight - controller.offset, 0.0);
+        final normalized = newOpacity.normalized(0.0, imgHeight).toDouble();
+        opacity.value = normalized;
+        size.value = normalized;
+      });
+      return null;
+    }, [controller]);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Hooks'),
       ),
-      body: Center(
-        child: Text(
-          notifier.value.toString(),
-          style: TextStyle(fontSize: 100),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizeTransition(
+            sizeFactor: size,
+            axis: Axis.vertical,
+            axisAlignment: -1.0,
+            child: FadeTransition(
+              opacity: opacity,
+              child: Image.network(
+                url,
+                width: double.infinity,
+                height: imgHeight.toDouble(),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              controller: controller,
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Person: ${index + 1}'),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }

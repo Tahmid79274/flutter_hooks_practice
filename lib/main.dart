ef64e37479c7c125275f21b601d29bb0747ae90e
Tahmid. <tahmidur.rahman.79274@gmail.com>
2024-07-06 22:48:24 +0600

    Application of useStreamController

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'dart:developer' as developer;
 
 void main() {
   runApp(const MyApp());
@@ -24,81 +25,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
-final url = 'https://rb.gy/ggnngr';
-const imgHeight = 300;
-
-extension Normalize on num {
-  num normalized(num selfRangeMin, num selfRangeMax,
-          [num normalizedRangeMin = 0.0, num normalizedRangeMax = 1.0]) =>
-      (normalizedRangeMax - normalizedRangeMin) *
-          ((this - selfRangeMin) / (selfRangeMax - normalizedRangeMax)) +
-      normalizedRangeMin;
-}
+const url = 'https://rb.gy/ggnngr';
 
 class MyHomePage extends HookWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final opacity = useAnimationController(
-        duration: const Duration(seconds: 1),
-        initialValue: 1.0,
-        upperBound: 1.0,
-        lowerBound: 0.0);
-
-    final size = useAnimationController(
-        duration: const Duration(seconds: 1),
-        initialValue: 1.0,
-        upperBound: 1.0,
-        lowerBound: 0.0);
-
-    final controller = useScrollController();
-
-    useEffect(() {
-      controller.addListener(() {
-        print('Add Listener Called');
-        final newOpacity = max(imgHeight - controller.offset, 0.0);
-        final normalized = newOpacity.normalized(0.0, imgHeight).toDouble();
-        opacity.value = normalized;
-        size.value = normalized;
-      });
-      return null;
-    }, [controller]);
+    late final StreamController<double> controller;
+    controller = useStreamController<double>(onListen: () {
+      developer.log('Tapped');
+      controller.sink.add(0.0);
+    });
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Hooks'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SizeTransition(
-            sizeFactor: size,
-            axis: Axis.vertical,
-            axisAlignment: -1.0,
-            child: FadeTransition(
-              opacity: opacity,
-              child: Image.network(
-                url,
-                width: double.infinity,
-                height: imgHeight.toDouble(),
-                fit: BoxFit.contain,
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              controller: controller,
-              itemCount: 100,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('Person: ${index + 1}'),
-                );
-              },
-            ),
-          )
-        ],
-      ),
+      body: StreamBuilder<double>(
+          stream: controller.stream,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              final rotation = snapshot.data ?? 0.0;
+              return InkWell(
+                onTap: () {
+                  controller.sink.add(rotation + 10);
+                },
+                child: RotationTransition(
+                    turns: AlwaysStoppedAnimation(rotation / 360),
+                    child: Center(child: Image.network(url))),
+              );
+            }
+          }),
     );
   }
 }
